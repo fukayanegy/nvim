f1de2747017490e1f5534d2e7059c168d06e83f0
fukayanegy <kakigoori00007@gmail.com>
2024-04-30 10:12:01 +0900

    bafa control command reset

--- lua/lazyvim/config.lua
+++ lua/lazyvim/config.lua
@@ -46,10 +46,6 @@ require('lazy').setup({
                 "<C-g>",
                 function() require("telescope.builtin").grep_string() end,
             },
-            {
-                "<C-b>",
-                function() require("telescope.builtin").buffers() end,
-            },
         },
         opts = {
             defaults = {
@@ -98,7 +94,7 @@ require('lazy').setup({
                     dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
                     dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
                     dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-                    dashboard.button("c", " " .. " Config",          "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+                    dashboard.button("c", " " .. " Config",          "<cmd> e ~/.config/nvim <cr>"),
                     dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
                     dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
                     dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
