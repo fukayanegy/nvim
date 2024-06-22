3c83460ab7cad11f181ca494b9c8ee63e2fdd878
fukayanegy <kakigoori00007@gmail.com>
2024-06-22 22:40:04 +0900

    after break

--- lua/lazyvim/config.lua
+++ lua/lazyvim/config.lua
@@ -79,10 +79,10 @@ require('lazy').setup({
             opts = function()
                 local dashboard = require("alpha.themes.dashboard")
                 local logo = [[
-                         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-                         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-                         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-                         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+                         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗
+                         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║    
+                         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║       
+                         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║         
                          ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
                          ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
                 ]]
@@ -168,11 +168,7 @@ require('lazy').setup({
             -- add any options here
         },
         dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         }
     },
