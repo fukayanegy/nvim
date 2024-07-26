67aa036913c305ad2a2367f00bdc542ae57baa14
fukayanegy <kakigoori00007@gmail.com>
2024-07-27 05:51:41 +0900

    dashboard

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,17 +1,31 @@
-config = function()
-    require('dashboard').setup {
-        center = {
+require('dashboard').setup {
+    theme = 'hyper',
+    config = {
+        week_header = {
+            enable = true,
+        },
+        shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
             {
-                icon = '',
-                icon_hl = 'group',
-                desc = 'description',
-                desc_hl = 'group',
-                key = 'shortcut key in dashboard buffer not keymap !!',
-                key_hl = 'group',
-                key_format = ' [%s]', -- `%s` will be substituted with value of `key`
-                action = '',
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'f',
+            },
+            {
+                desc = ' Apps',
+                group = 'DiagnosticHint',
+                action = 'Telescope app',
+                key = 'a',
+            },
+            {
+                desc = ' dotfiles',
+                group = 'Number',
+                action = 'Telescope dotfiles',
+                key = 'd',
             },
         },
-        footer = {},
-    }
-end
+    },
+}
